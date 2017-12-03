c22196ddc17a1f5486027b125216862b99fbb2c1
Jessica Bensch <jessica.bensch11@gmail.com>
2017-12-03 11:35:04 -0500

    Done.

---
+++ app/controllers/figures_controller.rb
@@ -0,0 +1,18 @@
+class FiguresController < ApplicationController
+
+  get '/figures' do
+    @figures = Figure.all
+    erb :'figures/index'
+  end
+
+  get '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :'figures/new'
+  end
+
+  post '/figures' do
+    @figure = Figure.create(params)
+
+  end
+end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,16 @@
 class LandmarksController < ApplicationController
 
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :'landmarks/index'
+  end
+
+  get '/landmarks/new' do
+    erb :'landmarks/new'
+  end
+
+  post 'landmarks' do
+    @landmark = Landmark.new(name: params[:name])
+  end
+
 end
